cd5a0b9cc8f650b36ef30e07dbf69ea2b43d7c0d
Ekwueme Nelson <an.ekwuemem@stu.unizik.edu.ng>
2024-01-24 19:27:53 +0100

    initial commit for change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold flex justify-center">NavBar</h1>
+      <h1 className="text-3xl flex justify-center">NavBar</h1>
     </div>
   );
 };
