6f234a8f2de30311e8e0f2dbb189d36e57772485
Ekwueme Nelson <an.ekwuemem@stu.unizik.edu.ng>
2024-01-30 00:09:03 +0100

    modification on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Set the target for the final output
-    target: 'modules',
-    // Specify the output directory for the production build
-    outDir: 'dist',
-    // Enable minification for the production build
-    minify: 'terser',
-    // Generate source maps for debugging
-    sourcemap: false,
-    // Inline dynamic imports for better performance
-  },
 });
